30d5175b4ef3853ace70990806f1f43ffa524bf1
M93 <m93.pl@protonmail.com>
2025-04-14 20:05:26 +0200

    Dodaj pliki projektów.

---
+++ Calculator.Designer.cs
@@ -0,0 +1,396 @@
+﻿namespace CSharp_Calculator
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            wynik = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            SuspendLayout();
+            // 
+            // wynik
+            // 
+            wynik.BackColor = Color.MediumPurple;
+            wynik.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            wynik.Location = new Point(29, 25);
+            wynik.Name = "wynik";
+            wynik.Size = new Size(561, 114);
+            wynik.TabIndex = 0;
+            wynik.Text = "000000000";
+            wynik.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.SlateBlue;
+            button1.BackgroundImageLayout = ImageLayout.Center;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button1.Location = new Point(32, 319);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 120);
+            button1.TabIndex = 1;
+            button1.Text = "7";
+            button1.TextAlign = ContentAlignment.TopRight;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.SlateBlue;
+            button2.BackgroundImageLayout = ImageLayout.Center;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button2.Location = new Point(32, 461);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 120);
+            button2.TabIndex = 2;
+            button2.Text = "4";
+            button2.TextAlign = ContentAlignment.TopRight;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.SlateBlue;
+            button3.BackgroundImageLayout = ImageLayout.Center;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button3.Location = new Point(164, 319);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 120);
+            button3.TabIndex = 3;
+            button3.Text = "8";
+            button3.TextAlign = ContentAlignment.TopRight;
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.SlateBlue;
+            button4.BackgroundImageLayout = ImageLayout.Center;
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button4.Location = new Point(164, 461);
+            button4.Name = "button4";
+            button4.Size = new Size(112, 120);
+            button4.TabIndex = 4;
+            button4.Text = "5";
+            button4.TextAlign = ContentAlignment.TopRight;
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.SlateBlue;
+            button5.BackgroundImageLayout = ImageLayout.Center;
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button5.Location = new Point(298, 461);
+            button5.Name = "button5";
+            button5.Size = new Size(112, 120);
+            button5.TabIndex = 5;
+            button5.Text = "6";
+            button5.TextAlign = ContentAlignment.TopRight;
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.SlateBlue;
+            button6.BackgroundImageLayout = ImageLayout.Center;
+            button6.FlatAppearance.BorderSize = 0;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button6.Location = new Point(298, 319);
+            button6.Name = "button6";
+            button6.Size = new Size(112, 120);
+            button6.TabIndex = 6;
+            button6.Text = "9";
+            button6.TextAlign = ContentAlignment.TopRight;
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.SlateBlue;
+            button7.BackgroundImageLayout = ImageLayout.Center;
+            button7.FlatAppearance.BorderSize = 0;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button7.Location = new Point(32, 607);
+            button7.Name = "button7";
+            button7.Size = new Size(112, 120);
+            button7.TabIndex = 7;
+            button7.Text = "1";
+            button7.TextAlign = ContentAlignment.TopRight;
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.SlateBlue;
+            button8.BackgroundImageLayout = ImageLayout.Center;
+            button8.FlatAppearance.BorderSize = 0;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button8.Location = new Point(164, 607);
+            button8.Name = "button8";
+            button8.Size = new Size(112, 120);
+            button8.TabIndex = 8;
+            button8.Text = "2";
+            button8.TextAlign = ContentAlignment.TopRight;
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.SlateBlue;
+            button9.BackgroundImageLayout = ImageLayout.Center;
+            button9.FlatAppearance.BorderSize = 0;
+            button9.FlatStyle = FlatStyle.Flat;
+            button9.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button9.Location = new Point(298, 607);
+            button9.Name = "button9";
+            button9.Size = new Size(112, 120);
+            button9.TabIndex = 9;
+            button9.Text = "3";
+            button9.TextAlign = ContentAlignment.TopRight;
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.SlateBlue;
+            button10.BackgroundImageLayout = ImageLayout.Center;
+            button10.FlatAppearance.BorderSize = 0;
+            button10.FlatStyle = FlatStyle.Flat;
+            button10.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button10.Location = new Point(164, 756);
+            button10.Name = "button10";
+            button10.Size = new Size(112, 120);
+            button10.TabIndex = 10;
+            button10.Text = "0";
+            button10.TextAlign = ContentAlignment.TopRight;
+            button10.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.SlateBlue;
+            button11.BackgroundImageLayout = ImageLayout.Center;
+            button11.FlatAppearance.BorderSize = 0;
+            button11.FlatStyle = FlatStyle.Flat;
+            button11.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button11.Location = new Point(298, 756);
+            button11.Name = "button11";
+            button11.Size = new Size(112, 120);
+            button11.TabIndex = 11;
+            button11.Text = ".";
+            button11.TextAlign = ContentAlignment.TopRight;
+            button11.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.SlateBlue;
+            button12.BackgroundImageLayout = ImageLayout.Center;
+            button12.FlatAppearance.BorderSize = 0;
+            button12.FlatStyle = FlatStyle.Flat;
+            button12.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button12.ImageAlign = ContentAlignment.TopCenter;
+            button12.Location = new Point(29, 756);
+            button12.Name = "button12";
+            button12.Size = new Size(112, 120);
+            button12.TabIndex = 12;
+            button12.Text = "%";
+            button12.TextAlign = ContentAlignment.TopLeft;
+            button12.UseVisualStyleBackColor = false;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.SlateBlue;
+            button13.BackgroundImageLayout = ImageLayout.Center;
+            button13.FlatAppearance.BorderSize = 0;
+            button13.FlatStyle = FlatStyle.Flat;
+            button13.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button13.Location = new Point(478, 319);
+            button13.Name = "button13";
+            button13.Size = new Size(112, 120);
+            button13.TabIndex = 13;
+            button13.Text = "+";
+            button13.TextAlign = ContentAlignment.TopRight;
+            button13.UseVisualStyleBackColor = false;
+            // 
+            // button14
+            // 
+            button14.BackColor = Color.SlateBlue;
+            button14.BackgroundImageLayout = ImageLayout.Center;
+            button14.FlatAppearance.BorderSize = 0;
+            button14.FlatStyle = FlatStyle.Flat;
+            button14.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button14.Location = new Point(478, 461);
+            button14.Name = "button14";
+            button14.Size = new Size(112, 120);
+            button14.TabIndex = 14;
+            button14.Text = "-";
+            button14.TextAlign = ContentAlignment.TopRight;
+            button14.UseVisualStyleBackColor = false;
+            // 
+            // button15
+            // 
+            button15.BackColor = Color.SlateBlue;
+            button15.BackgroundImageLayout = ImageLayout.Center;
+            button15.FlatAppearance.BorderSize = 0;
+            button15.FlatStyle = FlatStyle.Flat;
+            button15.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button15.Location = new Point(478, 607);
+            button15.Name = "button15";
+            button15.Size = new Size(112, 120);
+            button15.TabIndex = 15;
+            button15.Text = "x";
+            button15.TextAlign = ContentAlignment.TopRight;
+            button15.UseVisualStyleBackColor = false;
+            // 
+            // button16
+            // 
+            button16.BackColor = Color.SlateBlue;
+            button16.BackgroundImageLayout = ImageLayout.Center;
+            button16.FlatAppearance.BorderSize = 0;
+            button16.FlatStyle = FlatStyle.Flat;
+            button16.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button16.Location = new Point(478, 756);
+            button16.Name = "button16";
+            button16.Size = new Size(112, 120);
+            button16.TabIndex = 16;
+            button16.Text = "/";
+            button16.TextAlign = ContentAlignment.TopRight;
+            button16.UseVisualStyleBackColor = false;
+            // 
+            // button17
+            // 
+            button17.BackColor = Color.YellowGreen;
+            button17.BackgroundImageLayout = ImageLayout.Center;
+            button17.FlatAppearance.BorderSize = 0;
+            button17.FlatStyle = FlatStyle.Flat;
+            button17.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button17.Location = new Point(32, 174);
+            button17.Name = "button17";
+            button17.Size = new Size(378, 120);
+            button17.TabIndex = 17;
+            button17.Text = "=";
+            button17.UseVisualStyleBackColor = false;
+            // 
+            // button18
+            // 
+            button18.BackColor = Color.DarkViolet;
+            button18.BackgroundImageLayout = ImageLayout.Center;
+            button18.FlatAppearance.BorderSize = 0;
+            button18.FlatStyle = FlatStyle.Flat;
+            button18.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button18.ImageAlign = ContentAlignment.TopCenter;
+            button18.Location = new Point(478, 173);
+            button18.Name = "button18";
+            button18.Size = new Size(112, 120);
+            button18.TabIndex = 18;
+            button18.Text = "C";
+            button18.TextAlign = ContentAlignment.TopLeft;
+            button18.UseVisualStyleBackColor = false;
+            // 
+            // MainWindow
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            AutoScroll = true;
+            BackColor = Color.Indigo;
+            ClientSize = new Size(618, 904);
+            Controls.Add(button18);
+            Controls.Add(button17);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(wynik);
+            Cursor = Cursors.Hand;
+            Font = new Font("Consolas", 20F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            ForeColor = Color.White;
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(7, 6, 7, 6);
+            MaximizeBox = false;
+            Name = "MainWindow";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Calculator by M93.pl";
+            Load += MainWindow_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label wynik;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button button18;
+    }
+}
---
+++ Calculator.cs
@@ -0,0 +1,15 @@
+namespace CSharp_Calculator
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
